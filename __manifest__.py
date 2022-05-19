3766d02426c3816a17e9c5769600b2de5f2ec6e8
jwaes <jaco.waes@gmail.com>
2022-05-19 07:12:54 +0000

    Added video snippets

--- __manifest__.py
+++ __manifest__.py
@@ -2,13 +2,13 @@
     'name': 'NOW theme',
     'description': 'NOW Theme',
     'category': 'Theme/Services',
-    'version': '15.1.17',
+    'version': '15.1.20',
 
     'author': "jaco tech",
     'website': "https://jaco.tech",
     "license": "AGPL-3",
     
-    'depends': ['theme_common'],
+    'depends': ['theme_common', 'website', 'website_sale'],
     'data': [
         'data/ir_asset.xml',
         'views/customizations.xml',
@@ -18,7 +18,8 @@
         'views/snippets/s_text_block.xml',
         'views/snippets/s_text_image.xml',
         'views/snippets/s_image_text.xml',
-
+        'views/snippets/s_video_169.xml',
+        'views/snippets/s_video_43.xml',
     ],
     'images': [
     ],
@@ -27,12 +28,18 @@
     },
     'license': 'LGPL-3',
     'assets': {
+        'web.assets_qweb': [
+            'theme_now/static/src/xml/editor.xml',
+        ],        
         'website.assets_editor': [
             'theme_now/static/src/js/tour.js',
         ],
         'web.assets_frontend': [
             'theme_now/static/src/css/custom.css',
             'theme_now/static/lib/mdi/css/materialdesignicons.min.css',
+        ],
+        'website.assets_wysiwyg': [
+            'theme_now/static/src/js/snippets.editor.js',
         ],
     }
 }
---
+++ static/src/js/snippets.editor.js
@@ -0,0 +1,12 @@
+odoo.define('web_editor.now_override', function (require) {
+    'use strict';
+
+    // const weSnippetEditor = require('web_editor.snippet.editor');
+
+    // // alert("hellow");
+    // weSnippetEditor.SnippetsMenu.include({
+    //     xmlDependencies: (weSnippetEditor.SnippetsMenu.prototype.xmlDependencies || [])
+    //         .concat(['/theme_now/static/src/xml/editor.xml']),
+    // })
+
+});
