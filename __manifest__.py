a1a9c55572c1c53a3ad44c14dc547745895fc650
jwaes <jaco.waes@gmail.com>
2022-06-07 08:54:41 +0000

    version bump

--- __manifest__.py
+++ __manifest__.py
@@ -2,7 +2,7 @@
     'name': 'NOW theme',
     'description': 'NOW Theme',
     'category': 'Theme/Services',
-    'version': '1.23',
+    'version': '1.24',
 
     'author': "jaco tech",
     'website': "https://jaco.tech",
