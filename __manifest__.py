b4057d3f53e928b370c3c1753156bf201931d077
jwaes <jaco.waes@gmail.com>
2022-06-22 06:55:11 +0000

    added support css attr

--- __manifest__.py
+++ __manifest__.py
@@ -2,7 +2,7 @@
     'name': 'NOW theme',
     'description': 'NOW Theme',
     'category': 'Theme/Services',
-    'version': '1.27',
+    'version': '1.28',
 
     'author': "jaco tech",
     'website': "https://jaco.tech",
