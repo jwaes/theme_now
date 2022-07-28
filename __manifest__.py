26293252fb0672d9912c50049d86cd4653b70d1d
jwaes <jaco.waes@gmail.com>
2022-07-28 12:11:42 +0000

    cleanup

--- __manifest__.py
+++ __manifest__.py
@@ -2,7 +2,7 @@
     'name': 'NOW theme',
     'description': 'NOW Theme',
     'category': 'Theme/Services',
-    'version': '1.42',
+    'version': '1.43',
 
     'author': "jaco tech",
     'website': "https://jaco.tech",
