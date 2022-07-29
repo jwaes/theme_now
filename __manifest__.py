55ec1101cb1d32524f34f4b5cfa56608dfc6f809
jwaes <jaco.waes@gmail.com>
2022-07-29 12:24:05 +0000

    spacing

--- __manifest__.py
+++ __manifest__.py
@@ -2,7 +2,7 @@
     'name': 'NOW theme',
     'description': 'NOW Theme',
     'category': 'Theme/Services',
-    'version': '1.43',
+    'version': '1.44',
 
     'author': "jaco tech",
     'website': "https://jaco.tech",
