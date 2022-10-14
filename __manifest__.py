fec22026cac8df136bdb71f670d07b3cc35cd9a3
jwaes <jaco.waes@gmail.com>
2022-10-14 11:22:18 +0000

    h3

--- __manifest__.py
+++ __manifest__.py
@@ -2,7 +2,7 @@
     'name': 'NOW theme',
     'description': 'NOW Theme',
     'category': 'Theme/Services',
-    'version': '1.54',
+    'version': '1.55',
 
     'author': "jaco tech",
     'website': "https://jaco.tech",
