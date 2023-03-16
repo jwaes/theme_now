5d19e3b9190a4268850b1ee356e17eaa626176e2
jwaes <jaco.waes@gmail.com>
2023-03-16 09:16:29 +0000

    mega menu

--- __manifest__.py
+++ __manifest__.py
@@ -2,7 +2,7 @@
     'name': 'NOW theme',
     'description': 'NOW Theme',
     'category': 'Theme/Services',
-    'version': '1.57',
+    'version': '1.58',
 
     'author': "jaco tech",
     'website': "https://jaco.tech",
