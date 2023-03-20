81ce6857d430e3ef19cceec73aea590a6fc1666a
jwaes <jaco.waes@gmail.com>
2023-03-20 10:07:14 +0000

    bubbles

--- __manifest__.py
+++ __manifest__.py
@@ -2,7 +2,7 @@
     'name': 'NOW theme',
     'description': 'NOW Theme',
     'category': 'Theme/Services',
-    'version': '1.59',
+    'version': '1.60',
 
     'author': "jaco tech",
     'website': "https://jaco.tech",
