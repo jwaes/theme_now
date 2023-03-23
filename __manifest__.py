8cbf9f0f1d9bee2462ee528ce3c5d5a690eb2983
jwaes <jaco.waes@gmail.com>
2023-03-23 15:34:04 +0000

    nav bgcolor

--- __manifest__.py
+++ __manifest__.py
@@ -2,7 +2,7 @@
     'name': 'NOW theme',
     'description': 'NOW Theme',
     'category': 'Theme/Services',
-    'version': '1.66',
+    'version': '1.67',
 
     'author': "jaco tech",
     'website': "https://jaco.tech",
