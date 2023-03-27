e6990871799c761fbbca40d96635382d5dfb489a
jwaes <jaco.waes@gmail.com>
2023-03-27 06:42:56 +0000

    pagination css

--- __manifest__.py
+++ __manifest__.py
@@ -2,7 +2,7 @@
     'name': 'NOW theme',
     'description': 'NOW Theme',
     'category': 'Theme/Services',
-    'version': '1.68',
+    'version': '1.69',
 
     'author': "jaco tech",
     'website': "https://jaco.tech",
