f3441bf94a894db5ec287aa7561de6a6c6b3e7ba
jwaes <jaco.waes@gmail.com>
2024-03-06 14:30:59 +0000

    bump tour

--- __manifest__.py
+++ __manifest__.py
@@ -2,7 +2,7 @@
     'name': 'NOW theme',
     'description': 'NOW Theme',
     'category': 'Theme/Services',
-    'version': '2.1',
+    'version': '2.2',
 
     'author': "jaco tech",
     'website': "https://jaco.tech",
